58df82cc7aca0270288952ab40a86eee5f992646
kocwng <idakusuma2705@gmail.com>
2023-03-14 06:58:22 +0700

    add request

--- features/reservation/reservationEntity.go
+++ features/reservation/reservationEntity.go
@@ -18,17 +18,9 @@ type ReservationCore struct {
 }
 
 type ReservationServiceInterface interface {
-	// GetAll() ([]ReservationCore, error)
-	// GetById(id uint) (ReservationCore, error)
 	CreateReservation(input ReservationCore) error
-	// Update(reservationCore ReservationCore, id uint) (ReservationCore, error)
-	// Delete(id uint) error
 }
 
 type ReservationDataInterface interface {
-	// SelectAll() ([]ReservationCore, error)
-	// SelectById(id uint) (ReservationCore, error)
 	CreateReservation(input ReservationCore) error
-	// Edit(reservationCore ReservationCore, id uint) error
-	// Destroy(id uint) error
 }

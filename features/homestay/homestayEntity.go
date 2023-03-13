b6cb2dd8ab92f2ed80bb494fa55de2908e4732ac
alfianadityads <alfianadityadwi@gmail.com>
2023-03-13 21:23:46 +0700

    chore: build homestay entity and all interface

---
+++ features/homestay/homestayEntity.go
@@ -0,0 +1,43 @@
+package homestay
+
+import (
+	"mime/multipart"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Core struct {
+	ID       uint
+	Name     string `validate:"required"`
+	Address  string `validate:"required"`
+	Phone    string `validate:"required"`
+	Price    string
+	Facility string
+	Image1   string
+	Image2   string
+	Image3   string
+}
+
+type HomestayHandler interface {
+	Add() echo.HandlerFunc
+	ShowAll() echo.HandlerFunc
+	ShowDetail() echo.HandlerFunc
+	Update() echo.HandlerFunc
+	Delete() echo.HandlerFunc
+}
+
+type HomestayService interface {
+	Add(userRole, token interface{}, fileData multipart.FileHeader, newHomestay Core) (Core, error)
+	ShowAll() ([]Core, error)
+	ShowDetail(homestayID uint) (Core, error)
+	Update(token interface{}, homestayID uint, fileData multipart.FileHeader, updateData Core) (Core, error)
+	Delete(token interface{}, homestayID uint) error
+}
+
+type HomestayData interface {
+	Add(userRole, userID uint, newHomestay Core) (Core, error)
+	ShowAll() ([]Core, error)
+	ShowDetail(homestayID uint) (Core, error)
+	Update(userID uint, homestayID uint, updateHomestay Core) (Core, error)
+	Delete(userID uint, homestayID uint) error
+}

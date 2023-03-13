039b0052c7a4181447f0eae6d9958c4607bf6286
alfianadityads <alfianadityadwi@gmail.com>
2023-03-13 14:36:05 +0700

    feat: build function handler for regist feature

---
+++ features/user/handler/handler.go
@@ -0,0 +1,66 @@
+package handler
+
+import (
+	"airbnb/features/user"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/labstack/echo/v4"
+)
+
+type userHandler struct {
+	srv user.UserService
+}
+
+func New(srv user.UserService) user.UserHandler {
+	return &userHandler{
+		srv: srv,
+	}
+}
+
+// Delete implements user.UserHandler
+func (*userHandler) Delete() echo.HandlerFunc {
+	panic("unimplemented")
+}
+
+// Login implements user.UserHandler
+func (*userHandler) Login() echo.HandlerFunc {
+	panic("unimplemented")
+}
+
+// Profile implements user.UserHandler
+func (*userHandler) Profile() echo.HandlerFunc {
+	panic("unimplemented")
+}
+
+// Register implements user.UserHandler
+func (uh *userHandler) Register() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		input := RegisterRequest{}
+		err := c.Bind(&input)
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, map[string]interface{}{"message": "input format incorrect"})
+		}
+
+		res, err := uh.srv.Register(*ReqToCore(input))
+		if err != nil {
+			if strings.Contains(err.Error(), "already") {
+				return c.JSON(http.StatusBadRequest, map[string]interface{}{"message": "email already registered"})
+			} else if strings.Contains(err.Error(), "is not min") {
+				return c.JSON(http.StatusBadRequest, map[string]interface{}{"message": "validate: password length minimum 3 character"})
+			} else if strings.Contains(err.Error(), "validate") {
+				return c.JSON(http.StatusBadRequest, map[string]interface{}{"message": err.Error()})
+			} else {
+				return c.JSON(http.StatusInternalServerError, map[string]interface{}{"message": "internal server error"})
+			}
+		}
+		log.Println(res)
+		return c.JSON(http.StatusCreated, map[string]interface{}{"message": "success create account"})
+	}
+}
+
+// Update implements user.UserHandler
+func (*userHandler) Update() echo.HandlerFunc {
+	panic("unimplemented")
+}

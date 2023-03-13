ba29d80ee5ee83818949c47f3d838acb5c6b564a
kocwng <idakusuma2705@gmail.com>
2023-03-13 22:56:44 +0700

    add homestay service

--- features/homestay/services/service.go
+++ features/homestay/services/service.go
@@ -2,7 +2,6 @@ package services
 
 import (
 	"airbnb/features/homestay"
-	"airbnb/features/user"
 	"airbnb/helper"
 	"errors"
 	"log"
@@ -11,24 +10,23 @@ import (
 )
 
 type homestayService struct {
-	// qry homestay.HomestayData
-	Data     homestay.HomestayService
+	Data homestay.HomestayData
+	// Data     homestay.HomestayService
 	// validate *validator.Validate
 }
 
 func New(data homestay.HomestayData) homestay.HomestayService {
 	return &homestayService{
-		Data:     data,
+		Data: data,
 		// validate: validator.New(),
 	}
 }
 
-func (hs *homestayService) Add(userRole user.Core, token interface{}, fileData multipart.FileHeader, newHomestay homestay.Core) (homestay.Core, error) {
-	
-	if userRole.Role != "hoster"{
-		return homestay.Core{}, errors.New("role option only: hoster")
-	}
+func (hs *homestayService) Add(token interface{}, fileData multipart.FileHeader, newHomestay homestay.Core) (homestay.Core, error) {
 
+	// if userRole.Role != "hoster"{
+	// 	return homestay.Core{}, errors.New("role option only: hoster")
+	//
 
 	userID := helper.ExtractToken(token)
 
@@ -44,10 +42,10 @@ func (hs *homestayService) Add(userRole user.Core, token interface{}, fileData m
 	newHomestay.Image2 = url
 	newHomestay.Image3 = url
 
-	res, err := hs.Data.Add(uint(userID), newHomestay)
+	res, err := hs.Data.Add("", uint(userID), newHomestay)
 
 	if err != nil {
-		log.Println("cannot post homestay", err.Error())
+		log.Println("cannot post book", err.Error())
 		return homestay.Core{}, errors.New("server error")
 	}
 
@@ -81,35 +79,38 @@ func (hs *homestayService) ShowDetail(homestayID uint) (homestay.Core, error) {
 	return res, nil
 }
 
-func (hs *homestayService) Update(token interface{}, homestayID uint, fileData multipart.FileHeader, updatedBook homestay.Core) (homestay.Core, error) {
+func (hs *homestayService) Update(token interface{}, homestayID uint, fileData multipart.FileHeader, updatedHomestay homestay.Core) (homestay.Core, error) {
 	userID := helper.ExtractToken(token)
 
 	if userID <= 0 {
-		return book.Core{}, errors.New("user not found")
+		return homestay.Core{}, errors.New("user not found")
 	}
 
 	url, err := helper.GetUrlImagesFromAWS(fileData)
 	if err != nil {
-		return book.Core{}, errors.New("validate: " + err.Error())
+		return homestay.Core{}, errors.New("validate: " + err.Error())
 	}
-	updatedBook.Image = url
-	res, err := buc.qry.Update(uint(userID), bookID, updatedBook)
+	updatedHomestay.Image1 = url
+	updatedHomestay.Image2 = url
+	updatedHomestay.Image3 = url
+
+	res, err := hs.Data.Update(uint(userID), homestayID, updatedHomestay)
 
 	if err != nil {
 		if strings.Contains(err.Error(), "not found") {
-			return book.Core{}, errors.New("data not found")
+			return homestay.Core{}, errors.New("data not found")
 		} else {
-			return book.Core{}, errors.New("internal server error")
+			return homestay.Core{}, errors.New("internal server error")
 		}
 	}
 
 	return res, nil
 }
 
-func (buc *bookUseCase) Delete(token interface{}, bookID uint) error {
+func (hs *homestayService) Delete(token interface{}, homestayID uint) error {
 	id := helper.ExtractToken(token)
 
-	err := buc.qry.Delete(uint(id), bookID)
+	err := hs.Data.Delete(uint(id), homestayID)
 
 	if err != nil {
 		log.Println("delete query error", err.Error())
@@ -117,21 +118,4 @@ func (buc *bookUseCase) Delete(token interface{}, bookID uint) error {
 	}
 
 	return nil
-}
-
-func (buc *bookUseCase) MyBook(token interface{}) ([]book.Core, error) {
-	id := helper.ExtractToken(token)
-	res, err := buc.qry.MyBook(uint(id))
-
-	if err != nil {
-		msg := ""
-		if strings.Contains(err.Error(), "not found") {
-			msg = "book not found"
-		} else {
-			msg = "there is a problem with the server"
-		}
-		return []book.Core{}, errors.New(msg)
-	}
-
-	return res, nil
 }

a110fb47da0fd14c79d507f9e25e05d2e602ac96
alfianadityads <alfianadityadwi@gmail.com>
2023-03-13 13:37:02 +0700

    chore: build user entity and all interface method

---
+++ features/user/userEntity.go
@@ -0,0 +1,52 @@
+package user
+
+import (
+	"mime/multipart"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Core struct {
+	ID       uint
+	Name     string
+	Email    string `validate:"required,email,unique"`
+	Password string `validate:"required"`
+	Role     string `validate:"required"`
+	Phone    string
+	Address  string
+	// Homestay []Homestay
+}
+
+// type Homestay struct {
+// 	ID              uint
+// 	Name            string
+// 	HomestayAddress string
+// 	Phone           string
+// 	Price           int
+// 	Facility        string
+// 	Image1          string
+// }
+
+type UserHandler interface {
+	Register() echo.HandlerFunc
+	Login() echo.HandlerFunc
+	Profile() echo.HandlerFunc
+	Update() echo.HandlerFunc
+	Delete() echo.HandlerFunc
+}
+
+type UserService interface {
+	Register(newUser Core) (Core, error)
+	Login(email, password string) (string, Core, error)
+	Profile(token interface{}) (Core, error)
+	Update(token interface{}, fileData multipart.FileHeader, updateData Core) (Core, error)
+	Delete(token interface{}) error
+}
+
+type UserData interface {
+	Register(newUser Core) (Core, error)
+	Login(email string) (Core, error)
+	Profile(userID uint) (Core, error)
+	Update(userID uint, updateData Core) (Core, error)
+	Delete(userID uint) error
+}
--- go.mod
+++ go.mod
@@ -11,7 +11,14 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/labstack/echo/v4 v4.10.2 // indirect
+	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/net v0.8.0 // indirect
 	gorm.io/gorm v1.23.8 // indirect
 )
 

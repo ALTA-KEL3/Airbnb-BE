1eb252d67a742d6c2c4b71ae19b88bd80241c009
kocwng <idakusuma2705@gmail.com>
2023-03-15 06:21:17 +0700

    add entity

--- features/reservation/data/model.go
+++ features/reservation/data/model.go
@@ -1,33 +1,33 @@
 package data
 
-import (
-	"time"
-	u "airbnb/features/user/data"
-	"airbnb/features/reservation"
-	h "airbnb/features/homestay/data"
+// import (
+// 	"time"
+// 	u "airbnb/features/user/data"
+// 	"airbnb/features/reservation"
+// 	h "airbnb/features/homestay/data"
 
 
-	"gorm.io/gorm"
-)
+// 	"gorm.io/gorm"
+// )
 
-type Reservation struct {
-	gorm.Model
-	Checkin    time.Time
-	Checkout   time.Time
-	TotalPrice float64
-	HomestayID uint
-	Homestay   h.Homestay
-	UserID     uint
-	User       u.User
-}
+// type Reservation struct {
+// 	gorm.Model
+// 	Checkin    time.Time
+// 	Checkout   time.Time
+// 	TotalPrice float64
+// 	HomestayID uint
+// 	Homestay   h.Homestay
+// 	UserID     uint
+// 	User       u.User
+// }
 
-func CoretoModel(dataCore reservation.ReservationCore) Reservation {
-	dataGorm := Reservation{
-		Checkin:    dataCore.Checkin,
-		Checkout:   dataCore.Checkout,
-		TotalPrice: dataCore.TotalPrice,
-		HomestayID: dataCore.HomestayID,
-		UserID:     dataCore.UserID,
-	}
-	return dataGorm
-}
+// func CoretoModel(dataCore reservation.ReservationCore) Reservation {
+// 	dataGorm := Reservation{
+// 		Checkin:    dataCore.Checkin,
+// 		Checkout:   dataCore.Checkout,
+// 		TotalPrice: dataCore.TotalPrice,
+// 		HomestayID: dataCore.HomestayID,
+// 		UserID:     dataCore.UserID,
+// 	}
+// 	return dataGorm
+// }
--- features/reservation/services/service.go
+++ features/reservation/services/service.go
@@ -1,43 +1,43 @@
 package service
 
-import (
-	"airbnb/features/reservation"
-	"airbnb/helper"
-	"errors"
-	"time"
-	// "github.com/go-playground/validator/v10"
-)
+// import (
+// 	"airbnb/features/reservation"
+// 	"airbnb/helper"
+// 	"errors"
+// 	"time"
+// 	// "github.com/go-playground/validator/v10"
+// )
 
-type ReservationService struct {
-	Data reservation.ReservationDataInterface
-	// validasi *validator.Validate
-}
+// type ReservationService struct {
+// 	Data reservation.ReservationDataInterface
+// 	// validasi *validator.Validate
+// }
 
-func NewReservation(data reservation.ReservationDataInterface) reservation.ReservationServiceInterface {
-	return &ReservationService{
-		Data: data,
-		// validate: validator.New(),
-	}
+// func NewReservation(data reservation.ReservationDataInterface) reservation.ReservationServiceInterface {
+// 	return &ReservationService{
+// 		Data: data,
+// 		// validate: validator.New(),
+// 	}
 
-}
+// }
 
-func (service *ReservationService) CreateReservation(input reservation.ReservationCore) (reservation.ReservationCore, error) {
-	var token interface{}
-	userID := helper.ExtractToken(token)
+// func (service *ReservationService) CreateReservation(token interface{}, totalPrice float64) (reservation.ReservationCore, string, error) {
+// 	// var token interface{}
+// 	userID := helper.ExtractToken(token)
 
-	if userID <= 0 {
-		return reservation.ReservationCore{}, errors.New("user not found")
-	}
-	res, err := service.Data.CreateReservation(input)
-	if err != nil {
-		return reservation.ReservationCore{}, errors.New("failed to add reservation")
-	}
-	return res, nil
-}
-func Checkin(start, end time.Time) float64 {
-	difference := start.Sub(end)
-	days := float64(difference.Hours() / 24)
+// 	if userID <= 0 {
+// 		return reservation.ReservationCore{}, errors.New("user not found")
+// 	}
+// 	res, err := service.Data.CreateReservation(input)
+// 	if err != nil {
+// 		return reservation.ReservationCore{}, errors.New("failed to add reservation")
+// 	}
+// 	return res, nil
+// }
+// func Checkin(start, end time.Time) float64 {
+// 	difference := start.Sub(end)
+// 	days := float64(difference.Hours() / 24)
 
-	return days
+// 	return days
 
-}
+// }

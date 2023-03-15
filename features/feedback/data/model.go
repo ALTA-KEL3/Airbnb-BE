2af6714271631dd3ce90120d97a03e8542880c79
alfianadityads <alfianadityadwi@gmail.com>
2023-03-16 01:39:42 +0700

    chore: make all fucntion in model feedback

--- features/feedback/data/model.go
+++ features/feedback/data/model.go
@@ -9,47 +9,61 @@ import (
 
 type Feedback struct {
 	gorm.Model
-	Rating     uint
-	Note       string
-	UserID     uint
-	User       User
+	Rating uint
+	Note   string
+	UserID uint
+	User   User
 	// Name       string
 	HomestayID uint
 	// Homestay   homestay.Homestay
-	Homestay Homestay
+	// Homestay Homestay
+	ReservationID uint
 }
 
 type User struct {
 	gorm.Model
-	Feedback []Feedback `gorm:"foreignKey:UserID"`
+	Name           string
+	Email          string
+	Phone          string
+	ProfilePicture string
+	// Feedback []Feedback `gorm:"foreignKey:UserID"`
 }
 type Homestay struct {
 	gorm.Model
 	// ID uint
-	// Name     string
-	Feedback []Feedback `gorm:"foreignKey:HomestayID"`
-}
-
-func FeedbackCoreToFeedback(dataCore feedback.FeedbackCore) Feedback {
-	feedbackGorm := Feedback{
-		Model:      gorm.Model{ID: dataCore.ID},
-		Rating:     dataCore.Rating,
-		Note:       dataCore.Note,
-		UserID:     dataCore.UserID,
-		// Name:       dataCore.Name,
-		HomestayID: dataCore.HomestayID,
-	}
-	return feedbackGorm
+	Name     string
+	UserID   uint
+	Rating   uint
+	Feedback string
+	// Feedback []Feedback `gorm:"foreignKey:HomestayID"`
 }
 
 func FeedbackDataToCore(data Feedback) feedback.FeedbackCore {
-	dataFeedback := feedback.FeedbackCore{
-		ID:         data.ID,
-		Rating:     data.Rating,
-		Note:       data.Note,
-		UserID:     data.UserID,
+	return feedback.FeedbackCore{
+		ID:     data.ID,
+		Rating: data.Rating,
+		Note:   data.Note,
+		UserID: data.UserID,
 		// Name:       data.Name,
-		HomestayID: data.HomestayID,
+		HomestayID:    data.HomestayID,
+		ReservationID: data.ReservationID,
+		User: feedback.User{
+			ID:             data.User.ID,
+			Name:           data.User.Name,
+			ProfilePicture: data.User.ProfilePicture,
+		},
 	}
-	return dataFeedback
+
+}
+
+func FeedbackCoreToFeedback(dataCore feedback.FeedbackCore) Feedback {
+	return Feedback{
+		Model:  gorm.Model{ID: dataCore.ID},
+		Rating: dataCore.Rating,
+		Note:   dataCore.Note,
+		UserID: dataCore.UserID,
+		// Name:       dataCore.Name,
+		HomestayID:    dataCore.HomestayID,
+		ReservationID: dataCore.ReservationID,
+	}
 }

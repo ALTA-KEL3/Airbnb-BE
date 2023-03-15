f9b5cf82368528b57e9fd0d1acc0416bacab6609
alfianadityads <alfianadityadwi@gmail.com>
2023-03-15 20:08:31 +0700

    feat: make handler function for feedback list

--- features/feedback/handler/response.go
+++ features/feedback/handler/response.go
@@ -1,1 +1,23 @@
 package handler
+
+import (
+	"airbnb/features/feedback"
+)
+
+type FeedbackResp struct {
+	ID         uint   `json:"id"`
+	Rating     uint   `json:"rating"`
+	Note       string `json:"note"`
+	UserID     uint   `json:"user_id"`
+	HomestayID uint   `json:"homestay_id"`
+}
+
+func FeedbackResponse(data feedback.FeedbackCore) FeedbackResp {
+	return FeedbackResp{
+		ID:         data.ID,
+		Rating:     data.Rating,
+		Note:       data.Note,
+		UserID:     data.UserID,
+		HomestayID: data.HomestayID,
+	}
+}

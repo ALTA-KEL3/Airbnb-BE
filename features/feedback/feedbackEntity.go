3736a1edf4bac4b3fe9f7ec1e81bfbd27d4edea1
alfianadityads <alfianadityadwi@gmail.com>
2023-03-16 00:58:10 +0700

    chore: add struct user in feedback entity

--- features/feedback/feedbackEntity.go
+++ features/feedback/feedbackEntity.go
@@ -3,18 +3,26 @@ package feedback
 import "github.com/labstack/echo/v4"
 
 type FeedbackCore struct {
-	ID         uint
-	Rating     uint
-	Note       string
-	UserID     uint
+	ID     uint
+	Rating uint
+	Note   string
+	UserID uint
 	// Name       string
-	HomestayID uint
+	HomestayID    uint
+	ReservationID uint
+	User          User
 }
 
-type Homestay struct {
-	ID       uint
-	// Name     string
-	Feedback []FeedbackCore
+// type Homestay struct {
+// 	ID uint
+// 	// Name     string
+// 	Feedback []FeedbackCore
+// }
+
+type User struct {
+	ID             uint
+	Name           string
+	ProfilePicture string
 }
 
 type FeedbackHandler interface {
@@ -31,4 +39,3 @@ type FeedbackDataInterface interface {
 	AddFeedback(userID uint, newFeedback FeedbackCore) (FeedbackCore, error)
 	ListFeedback(userID uint, homestayID uint) ([]FeedbackCore, error)
 }
-

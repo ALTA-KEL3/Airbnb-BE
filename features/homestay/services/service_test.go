599d77a27171e98eebd6cad5b3f84ec8641b648d
alfianadityads <alfianadityadwi@gmail.com>
2023-03-17 01:16:11 +0700

    test: make function testing for update homestay

--- features/homestay/services/service_test.go
+++ features/homestay/services/service_test.go
@@ -86,3 +86,86 @@ func TestAdd(t *testing.T) {
 	})
 
 }
+
+func TestUpdate(t *testing.T) {
+	repo := mocks.NewHomestayData(t)
+	filePath := filepath.Join("..", "..", "..", "ERD.png")
+	imageTrue, err := os.Open(filePath)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	imageTrueCnv := &multipart.FileHeader{
+		Filename: imageTrue.Name(),
+	}
+
+	inputData := homestay.Core{
+		ID:       1,
+		Name:     "villa 1",
+		Address:  "Jogja",
+		Phone:    "081234567",
+		Price:    500000,
+		Facility: "5 Kamar Tidur, 2 Kamar Mandi dalam, AC, Private Pool, dan Rooftop",
+	}
+
+	resData := homestay.Core{
+		ID:       1,
+		Name:     "villa 1",
+		Address:  "Jogja",
+		Phone:    "081234567",
+		Price:    500000,
+		Facility: "5 Kamar Tidur, 2 Kamar Mandi dalam, AC, Private Pool, dan Rooftop",
+	}
+
+	t.Run("success update homestay", func(t *testing.T) {
+		repo.On("Update", uint(1), uint(1), inputData).Return(resData, nil).Once()
+		srv := New(repo)
+		_, token := helper.GenerateToken(1)
+		pToken := token.(*jwt.Token)
+		pToken.Valid = true
+		res, err := srv.Update(pToken, uint(1), *imageTrueCnv, inputData)
+		assert.Nil(t, err)
+		assert.Equal(t, resData.ID, res.ID)
+		repo.AssertExpectations(t)
+	})
+
+	t.Run("data not found", func(t *testing.T) {
+		repo.On("Update", uint(1), uint(1), inputData).Return(homestay.Core{}, errors.New("data not found")).Once()
+		srv := New(repo)
+
+		_, token := helper.GenerateToken(1)
+		pToken := token.(*jwt.Token)
+		pToken.Valid = true
+		res, err := srv.Update(pToken, uint(1), *imageTrueCnv, inputData)
+		assert.NotNil(t, err)
+		assert.Equal(t, uint(0), res.ID)
+		assert.ErrorContains(t, err, "not found")
+		repo.AssertExpectations(t)
+	})
+
+	t.Run("invalid jwt", func(t *testing.T) {
+		srv := New(repo)
+
+		_, token := helper.GenerateToken(0)
+
+		pToken := token.(*jwt.Token)
+		pToken.Valid = true
+
+		_, err := srv.Update(pToken, uint(1), *imageTrueCnv, inputData)
+		assert.NotNil(t, err)
+		assert.ErrorContains(t, err, "found")
+		repo.AssertExpectations(t)
+	})
+
+	t.Run("server problem", func(t *testing.T) {
+		repo.On("Update", uint(1), uint(1), inputData).Return(homestay.Core{}, errors.New("server problem")).Once()
+		srv := New(repo)
+		_, token := helper.GenerateToken(1)
+		pToken := token.(*jwt.Token)
+		pToken.Valid = true
+		res, err := srv.Update(pToken, uint(1), *imageTrueCnv, inputData)
+		assert.NotNil(t, err)
+		assert.ErrorContains(t, err, "server")
+		assert.NotEqual(t, 0, res.ID)
+		repo.AssertExpectations(t)
+	})
+}

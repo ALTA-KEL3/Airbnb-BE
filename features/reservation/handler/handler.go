46c0656ffa336909c82f91a6ca007f325bb5bb4c
kocwng <idakusuma2705@gmail.com>
2023-03-16 07:49:09 +0700

    update handler response format

--- features/reservation/handler/handler.go
+++ features/reservation/handler/handler.go
@@ -1,14 +1,8 @@
 package handler
 
-// package delivery
-
 import (
-	// "errors"
-	// "log"
-	"math"
-	"net/http"
 
-	// "strconv"
+	"net/http"
 
 	"airbnb/features/reservation"
 	"airbnb/features/user"
@@ -16,15 +10,6 @@ import (
 
 	"github.com/labstack/echo/v4"
 )
-
-// type ReservationDelivery struct {
-// 	reservationService reservation.ReservationServiceInterface
-// }
-
-// func New(service reservation.ReservationServiceInterface, e *echo.Echo) {
-// 	handler := &ReservationDelivery{
-// 		reservationService: service,
-// 	}
 
 type reservationHandler struct {
 	srv reservation.ReservationServiceInterface
@@ -38,11 +23,6 @@ func New(srv reservation.ReservationServiceInterface, rsvSrvc user.UserService) 
 	}
 }
 
-// e.POST("/reservations/check", handler.CheckAvailability, middlewares.JWTMiddleware())
-// e.POST("/reservations", handler.Payment, middlewares.JWTMiddleware())
-// e.GET("/reservations", handler.GetHistory, middlewares.JWTMiddleware())
-// }
-// func (hh *homestayHandler) ShowAll() echo.HandlerFunc {
 func (d *reservationHandler) CheckAvailability() echo.HandlerFunc {
 
 	return func(c echo.Context) error {
@@ -58,45 +38,15 @@ func (d *reservationHandler) CheckAvailability() echo.HandlerFunc {
 			return c.JSON(http.StatusBadRequest, helper.ResponseFail("error read data"))
 		}
 
-		start := dataInput.Checkin
-		end := dataInput.Checkout
-		period := int(math.Ceil(end.Sub(start).Hours() / 24))
+		start := dataInput.Checkin.Format("2006-01-02")
+		end := dataInput.Checkout.Format("2006-01-02")
 
 		dataResponse := fromCoreAvail(res)
-		dataResponse.Duration = period
-		dataResponse.TotalPrice = dataInput.Duration * dataResponse.Price
+		dataResponse.HomestayID = dataInput.HomestayID
+		dataResponse.Checkin = start
+		dataResponse.Checkout = end
 
 		return c.JSON(http.StatusOK, helper.ResponseSuccess("available reservation", dataResponse))
 
 	}
 }
-
-// func (d *ReservationDelivery) Payment(c echo.Context) error {
-// 	idUser := middlewares.ExtractTokenUserId(c)
-// 	input := PaymentRequest{}
-// 	errBind := c.Bind(&input)
-// 	if errBind != nil {
-// 		return c.JSON(http.StatusNotFound, helper.FailedResponse("requested resource was not found"+errBind.Error()))
-// 	}
-
-// 	input.UserID = uint(idUser)
-// 	dataInput := ToCorePayment(input)
-// 	err := d.reservationService.CreatePayment(dataInput)
-// 	if err != nil {
-// 		return c.JSON(http.StatusBadRequest, helper.FailedResponse("error read data"))
-// 	}
-// 	return c.JSON(http.StatusOK, helper.SuccessResponse("Success reservation, see you later"))
-// }
-
-// func (d *ReservationDelivery) GetHistory(c echo.Context) error {
-// 	idUser := middlewares.ExtractTokenUserId(c)
-// 	userId := uint(idUser)
-// 	results, err := d.reservationService.GetHistory(userId)
-// 	if err != nil {
-// 		return c.JSON(http.StatusBadRequest, helper.FailedResponse(err.Error()))
-// 	}
-
-// 	dataResponse := TripArr(results)
-
-// 	return c.JSON(http.StatusOK, helper.SuccessWithDataResponse("success get trip history.", dataResponse))
-// }
